3ea2e3f1b03573f7323315881f3f4b63b765e4fa
Jawad Boudarat <jawadboudarat@gmail.com>
2021-02-19 15:35:03 +0000

    v 1.1

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Logo></Logo>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Logo from "./components/Logo.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
-  }
+    Logo,
+  },
 };
 </script>
 
---
+++ src/components/Logo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="logo">
+    <img alt="logo" src="../assets/logo.png" />
+    <h1>Creación de ToDo's</h1>
+  </div>
+</template>
+<style scoped>
+.logo {
+  display: flex;
+  align-items: center;
+}
+img {
+  width: 50px;
+  margin-right: 1rem;
+}
+</style>
